e4b28db054dfb660cff408c5286caabb1153c382
vagrant <vagrant@vagrant.vm>
2020-08-15 03:26:43 +0000

    commit

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from slack import WebClient
 SLACK_TOKEN = os.environ["SLACK_API_TOKEN"]
 SLACK_CHANNEL = "#planter"
 
-url = "https://sfbay.craigslist.org/search/sss?query=pot+%7C+planter+%7C+raised+bed+%7C+garden+bed&sort=rel&hasPic=1"
+url = "https://sfbay.craigslist.org/search/sss?query=planter"
 
 headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
@@ -23,10 +23,10 @@ from bs4 import BeautifulSoup as b_s
 craigslist_soup = b_s(response.content, "html.parser")
 posts = craigslist_soup.find_all("li", class_="result-row")
 first_cl_result = posts[0]
-# print(posts[0])
+print(posts[0])
+
 first_cl_price = first_cl_result.a.text
 print(f"first result price is {first_cl_price}")
-
 first_cl_time_class = first_cl_result.find("time", class_="result-date")
 print(f"first craigslist time class is {first_cl_time_class}")
 first_cl_datetime = first_cl_time_class["datetime"]
@@ -41,15 +41,14 @@ first_cl_title_text = first_cl_title_class.text
 print(f" title is {first_cl_title_text}")
 first_cl_neighborhood = first_cl_result.find("span", class_="result-hood")
 print(first_cl_neighborhood.text)
+neighborhood = first_cl_neighborhood.text
 
 
 client = WebClient(SLACK_TOKEN)
 desc = f"{first_cl_price}"
-client.api_call(
-    "client.chat_postMessage",
+response = client.chat_postMessage(
     channel=SLACK_CHANNEL,
-    text=first_cl_id,
-    username="craigslist"
-    # icon_emoji=":robot_face:",
+    text=first_cl_title_text + first_cl_link + neighborhood + first_cl_price,
 )
+# print(response)
 
