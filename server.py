87e79eec745fba52aec75bc443ec5c5a4dfd7d84
vagrant <vagrant@vagrant.vm>
2020-08-18 03:49:42 +0000

    commit

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-# from slack import WebClient
+from slack import WebClient
 from bs4 import BeautifulSoup as b_s
 
 
@@ -28,29 +28,28 @@ def search_query(region, term):
         # print(f"title class is {title_class}")
         # print(title_class['href'])
         links.append(title_class["href"])
-        print(links)
+        # print(links)
         posting_body = []
-        for link in links:
-            print(links)
-            print(f"link is {link}")
-            response_link = requests.get(url=link)
-            # print(f"response link is {response_link}")
-            link_soup = b_s(response_link.content, "html.parser")
-            print(f"this is link soup {link_soup}")
-            section_body_class = link_soup.find("section", id="postingbody")
-            # print(f"this is section body class {section_body_class}")
-            print(section_body_class)
-            print(section_body_class.text)
-            item_description = "".join(section_body_class.text)
-            print(f"item description is {item_description}")
-            stripped = item_description.replace("\n\nQR Code Link to This Post\n", "")
-            final_strip = stripped.replace("\n\n", "")
-            # print(final_strip)
-            final_final_strip = final_strip.rstrip()
-            print(f"final final strip is{final_final_strip}")
-            # posting_body.append(section_body_class.text)
-            posting_body.append(final_final_strip)
-            # print(posting_body)
+    for link in links:
+        print(links)
+        # print(f"link is {link}")
+        response_link = requests.get(url=link)
+        link_soup = b_s(response_link.content, "html.parser")
+        print(f"this is link soup {link_soup}")
+        section_body_class = link_soup.find("section", id="postingbody")
+        print(f"section body class is {section_body_class}")
+        print(f"section body class text is {section_body_class.text}")
+        item_description = "".join(section_body_class.text)
+        print(f"item description is {item_description}")
+        stripped = item_description.replace("\n\nQR Code Link to This Post\n", "")
+        final_strip = stripped.replace("\n\n", "")
+        # print(final_strip)
+        final_final_strip = final_strip.rstrip()
+        print(f"final final strip is{final_final_strip}")
+        # posting_body.append(section_body_class.text)
+        posting_body.append(final_final_strip)
+        poop = "".join(posting_body)
+        # print(posting_body)
 
         # print(posting_body)
         # cl_description = link_soup.find("div", class_="print-qrcode")
@@ -77,7 +76,7 @@ def search_query(region, term):
             print(f"this is neighborhood {neighborhood}")
             neighborhood_text = neighborhood.text
             client = WebClient(SLACK_TOKEN)
-            desc = f" {result_price.text} |{title_text} | {datetime} | {link} | {neighborhood_text} | {(posting_body)}"
+            desc = f" {result_price.text} | {title_text} | {datetime} | {link} | {neighborhood_text} | {poop}"
             response = client.chat_postMessage(channel=SLACK_CHANNEL, text=desc)
             print(response)
 
