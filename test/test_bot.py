bb2cd82748a791e399f8afe6dc0df1f2e3487156
Chantel Yip <chantelyip422@gmail.com>
2021-05-02 22:20:27 -0700

    added test to check if slack api has a 200 response

--- test/test_bot.py
+++ test/test_bot.py
@@ -1,11 +1,19 @@
-from scraper import *
-
+# from scraper import *
 import unittest
 import requests
 
+
 class TestSlackBot(unittest.TestCase):
-    def test_ping_slack_api():
-        pass
+    def test_slack_api(self):
+        slack_url = "https://slack.com/api/api.test"
+        response = requests.get(slack_url)
+        result = response.status_code
+        return result
+        self.assertEqual(self.test_slack_api(), "200")
+
     def test_spellcheck():
         pass
-    def test_
+
+
+if __name__ == "__main__":
+    unittest.main()
